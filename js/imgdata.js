d79c4b072fb232e2b9642ec28f6fe2358e58212b
lncprkns <lance@lncprkns.com>
2023-02-06 20:09:42 -0600

    Initial commit

---
+++ js/imgdata.js
@@ -0,0 +1,89 @@
+const imageData = [
+    {
+        caption: " visited Acadia National Park! It was beautiful!",
+        image: "/img/acadia.jpg",
+        alt: "Kate and Lance at Acadia National Park."
+    },
+    {
+        caption: " took our picture in front of this sign in Bangor after getting a tasty bagel breakfast.",
+        image: "/img/bangor.jpg",
+        alt: "Kate and Lance in front of a Bangor sign with a lumberjack."
+    },
+    {
+        caption: " had a delicious steak dinner at Bern's! YUM!",
+        image: "/img/berns.jpg",
+        alt: "Kate and Lance dressed up for dinner at Bern's Steakhouse in Tampa, FL."
+    },    
+    {
+        caption: " took our picture waiting for an Uber in Chicago. We went and tried Nando's Peri Peri. Delicious!",
+        image: "/img/chicago.jpg",
+        alt: "Kate and Lance while on the sidewalk in Chicago."
+    },
+    {
+        caption: " visit Chicago most years around Christmas. It's always magical!",
+        image: "/img/chicagoxmas.jpg",
+        alt: "Kate and Lance by the Macy's Christmas display in Chicago."
+    },
+    {
+        caption: " walked the Calleja de las Flores in Cordoba. It was beautiful!",
+        image: "/img/cordoba.jpg",
+        alt: "Kate and Lance on the Calleja de las Flores (the strees of the flowers) in Cordoba, Spain."
+    },
+    {
+        caption: " kicked off our 2019 by seeing Fiddler on the Roof. It was excellent!",
+        image: "/img/fiddler.jpg",
+        alt: "Kate and Lance inside Cadillac Theater waiting for the show to begin."
+    },
+    {
+        caption: " always love visiting Indian Rocks Beach!",
+        image: "/img/florida.jpg",
+        alt: "Kate and Lance at Indian Rocks Beach in Florida."
+    },
+    {
+        caption: " had an amazing Italian dinner and then saw Hamilton! Wow what a show!!!",
+        image: "/img/hamilton.jpg",
+        alt: "Kate and Lance in front of the Hamilton marquee at Cadillac Theater in Chicago."
+    },
+    {
+        caption: " always enjoy sitting at our favorite spot at Krape Park!",
+        image: "/img/krape.jpg",
+        alt: "Kate and Lance at Krape Park in Illinois."
+    },
+    {
+        caption: " visited our first cave at Mammoth Cave National Park in Kentucky!",
+        image: "/img/kycave.jpg",
+        alt: "Kate and Lance in a cave at Mammoth Cave National Park."
+    },
+    {
+        caption: " got to see some amazing disc golf at Ledgestone!",
+        image: "/img/ledgestone.jpg",
+        alt: "Kate and Lance watching professional disc golf in Peoria, IL."
+    },
+    {
+        caption: " found this beautiful little park at the end of our Minnesota weekend getaway!",
+        image: "/img/mn.jpg",
+        alt: "Kate and Lance in front of a waterfall at a park."
+    },
+    {
+        caption: " saw some beautiful foliage on this Missippi River boat tour in Minnesota.",
+        image: "/img/mnboat.jpg",
+        alt: "Kate and Lance on a boat tour."
+    },
+    {
+        caption: " had an amazing time in Nice! One of my two favorite places we've ever been!",
+        image: "/img/nice.jpg",
+        alt: ""
+    },    
+    {
+        caption: " played probably my favorite disc golf course at Sabattus in Maine!",
+        image: "/img/sabattus.jpg",
+        alt: "Kate and Lance on the tee of hole 1 at the Hawk Disc Golf Course at Sabattus Disc Golf in Maine."
+    },
+    {
+        caption: " walked around the beautiful city of Seville, my other favorite place we've been!",
+        image: "/img/seville.jpg",
+        alt: "Kate and Lance in front of a water fountain in Seville, Spain."
+    }
+]
+
+export default imageData
---
+++ js/index.js
@@ -0,0 +1,86 @@
+import imageData from "./imgdata.js"
+import {BEACH_PIC_API_KEY, WEATHER_API_KEY } from "./apikeys.js"
+
+
+function displayImg() {
+    const selectImage = Math.floor(Math.random() * imageData.length)
+    const imgCaption = imageData[selectImage].caption
+    const imgSrc = imageData[selectImage].image
+    const altText = imageData[selectImage].alt
+    
+    document.getElementById('our-picture').innerHTML = `
+        
+        <img src="${imgSrc}" alt="${altText}">
+        <div id="caption-container">
+        <caption>We ${imgCaption}</caption>
+        </div>
+        `
+}
+
+displayImg()
+
+fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=61032&aqi=no`)
+    .then(res => res.json())
+    .then(currently => document.getElementById("current-weather").innerHTML = `
+    <p id="current-temp">Currently: ${Math.round(currently.current.temp_f)}°F</p>
+    <p id="real-feel">Real feel: ${Math.round(currently.current.feelslike_f)}°F</p>
+    <p id="humidity">Humidity: ${currently.current.humidity}%</p>
+    <p id="uv-index">UV Index: ${currently.current.uv}</p>
+    <p id="wind-speed">Wind: ${Math.round(currently.current.wind_mph)} mph</p>
+    <p id="wind-gusts">Gusts: ${Math.round(currently.current.gust_mph)} mph</p>
+    `)
+
+fetch(`https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=61032&days=3&aqi=no&alerts=no`)
+    .then(res => res.json())
+    .then(threeDayData => document.getElementById("weather-forecast").innerHTML = `
+    <p>Today:</p>
+    <p>Hi: ${Math.round(threeDayData.forecast.forecastday[0].day.maxtemp_f)}°F</p>
+    <p>Lo: ${Math.round(threeDayData.forecast.forecastday[0].day.mintemp_f)}°F</p>
+    <p>UV Index: ${threeDayData.forecast.forecastday[0].day.uv}</p>
+    <br>
+    <p>Tomorrow:</p>
+    <p>Hi: ${Math.round(threeDayData.forecast.forecastday[1].day.maxtemp_f)}°F</p>
+    <p>Lo: ${Math.round(threeDayData.forecast.forecastday[1].day.mintemp_f)}°F</p>
+    <p>UV Index: ${threeDayData.forecast.forecastday[1].day.uv}</p>
+    <br>
+    <p>The day after:</p>
+    <p>Hi: ${Math.round(threeDayData.forecast.forecastday[2].day.maxtemp_f)}°F</p>
+    <p>Lo: ${Math.round(threeDayData.forecast.forecastday[2].day.mintemp_f)}°F</p>
+    <p>UV Index: ${threeDayData.forecast.forecastday[2].day.uv}</p>
+    `)
+
+fetch("https://jservice.io/api/random")
+    .then(res => res.json())
+    .then(answer => document.getElementById("trivia").innerHTML = `
+    <p>Difficulty: $${answer[0].value}</p>
+    <p>Category: ${answer[0].category.title}</p>
+    <p>Question: ${answer[0].question}</p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <p id="answer">Answer: ${answer[0].answer}</p>
+    `)
+
+fetch("https://icanhazdadjoke.com/", {
+    headers: {
+        'Accept': 'application/json'
+    }})
+    .then(res => res.json())
+    .then(joke => document.getElementById("dad-joke").innerHTML = `
+    <p>${joke.joke}</p>
+    `)
+
+fetch(`https://api.unsplash.com/photos/random/?query=beach&client_id=${BEACH_PIC_API_KEY}`)
+    .then(res => res.json())
+    .then(data => document.getElementById("beach-picture").innerHTML = `
+    <img src="${data.urls.small}">
+    `)
