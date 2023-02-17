aa31141f6ca87171a00fc824816d2dac278d989b
lncprkns <lance@lncprkns.com>
2023-02-16 21:14:32 -0600

    fixing api imports

---
+++ js/apikeys.js
@@ -0,0 +1,3 @@
+export const BEACH_PIC_API_KEY = 'WPzIHClZOPBy6YnMJmE-gyi30cDbiVq8Fwn1W8X4iyo';
+export const WEATHER_API_KEY = '31ba2f41ecd643fbb4f180612230402';
+export const WORDNIK_API_KEY = 'gywnro7lnhcin6imwe76nj4azfdme1qu2qsxmnyzs57a61wyf';
